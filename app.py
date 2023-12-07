f9def091a079e6ad4e6f63bcb41763ad160c7108
StudentELvdH <lohmeijer.vandenhul@gmail.com>
2023-12-07 15:47:23 +0100

    bestanden.py

--- app.py
+++ app.py
@@ -28,24 +28,7 @@ def index():
     return "Project 1: TODO - deze stap uitwerken"
 
 @app.route("/>", methods=["GET"])
-def book(isbn):
-   
-    # Get form information.
-    #isbn = request.form.get("isbn")
-    #try:
-    #    isbn = str(request.form.get("isbn"))
-    #except ValueError:
-    #    return render_template("error.html", message="Invalid isbn")
-    # print(isbn)
-    
-    # Make sure isbn exists-> nu in ORM
-    #if Book.query.filter_by(isbn=isbn)
-    
-    # Make sure isbn exists.
-    #if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).rowcount == 0:
-    #    return render_template("error.html", message="No such exercise with that code")
-    #db.execute("INSERT INTO books (isbn) VALUES (:isbn)",
-    #        {"isbn": isbn})
+def exercise(twopeople):
+       
     db.commit()
     return render_template("exercise.html")
-
